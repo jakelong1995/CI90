49fc9e514b0dade22dab7a65080ba236951ca764
jakelong1995 <hoanglong2579@gmail.com>
2023-11-15 20:23:59 +0700

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      Hello CI90
+      <Header/>
+    </>  
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+const Header = () => {
+    return <h1>Header component</h1>
+}
+export default Header;
