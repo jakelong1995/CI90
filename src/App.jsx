92cc64812615e7496a2d5d46171eeb99ecea64f8
jakelong1995 <hoanglong2579@gmail.com>
2023-11-16 09:00:09 +0700

    16/11

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import AddTask from "./TaskManagement/AddTask";
 
 function App() {
   return (
-    <div className={bg-white}>
+    <>
       <AddTask/>
-    <div/>  
-  );
+    </>  
+  )
 }
 
 export default App;
