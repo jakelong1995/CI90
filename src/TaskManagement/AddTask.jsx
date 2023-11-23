a154767ea8234ba6667b645e73ee7fbf705a754c
jakelong1995 <hoanglong2579@gmail.com>
2023-11-23 09:22:53 +0700

    commit

--- src/TaskManagement/AddTask.jsx
+++ src/TaskManagement/AddTask.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState } from 'react';
 
-const AddTask = () => {
+export default function Form() {
+  const [person, setPerson] = useState({
+    firstName: 'Barbara',
+    lastName: 'Hepworth',
+    email: 'bhepworth@sculpture.com'
+  });
+
+  function handleFirstNameChange(e) {
+    setPerson({
+      ...person,
+      firstName: e.target.value
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPerson({
+      ...person,
+      lastName: e.target.value
+    });
+  }
+
+  function handleEmailChange(e) {
+    setPerson({
+      ...person,
+      email: e.target.value
+    });
+  }
+
   return (
-    <h1 className="text-3xl text-gray-600 font-normal p-4 bg-slate-50 rounded-lg"> Enter your task here...</h1>
-  )
+    <>
+      <label>
+        First name:
+        <input
+          value={person.firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          value={person.lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          value={person.email}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <p>
+        {person.firstName}{' '}
+        {person.lastName}{' '}
+        ({person.email})
+      </p>
+    </>
+  );
 }
-
-export default AddTask
