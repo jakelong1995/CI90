374c962928c35c3dbf05642d73018d1d661c38f9
jakelong1995 <hoanglong2579@gmail.com>
2023-11-24 23:58:43 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import AddTask from "./TaskManagement/AddTask";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <AddTask/>
+      <Form/>
     </>  
   )
 }
--- src/TaskManagement/CountTask.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CountTask() {
-  return (
-    <div>CountTask</div>
-  )
-}
-
-export default CountTask
