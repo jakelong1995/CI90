eba7920720a3872e7f2c2d13556e3d97948a7f3b
jakelong1995 <hoanglong2579@gmail.com>
2023-11-29 19:25:58 +0700

    s

--- src/BigAssignment/Form.jsx
+++ src/BigAssignment/Form.jsx
@@ -1,13 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Form = () => {
+export default function AddForm() {
+  const initData = { name: '', amount: '', date: '' };
+  const [isAdded, setIsAdded] = useState(false);
+  const [expenseInfo, setexpenseInfo] = useState(initData);
+  const { name, amount, date } = expenseInfo;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setexpenseInfo({ ...expenseInfo, [name]: value });
+  };
+
+  const handleAdd = () => {
+    console.log(name);
+    if (!name || !amount || !date) {
+      alert('Empty fields, please input all the fields!');
+    } else {
+      setexpenseInfo(initData);
+    }
+  };
+
+  const handleChangeStatusAdd = () => setIsAdded((prev) => !prev);
+
+  const handleCancel = () => {
+    handleChangeStatusAdd();
+    setexpenseInfo(initData);
+  };
+
   return (
-    <form className='bg-indigo-100 h-[400px] w-[400px] justify-center'>
-      <button>
-        Add New Expense
-      </button>
-    </form>
-  )
-}
+    <div className='flex items-center justify-center h-screen'>
+      <div className='p-6 bg-gray-100 rounded-md w-96 flex justify-center items-center'>
+        {!isAdded ? (
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            onClick={() => handleChangeStatusAdd()}
+          >
+            Add Expense
+          </button>
+        ) : (
+          <form className='space-y-4'>
+            <div>
+              <label htmlFor='name' className='block text-sm font-medium text-gray-700'>
+                Name
+              </label>
+              <input
+                type='text'
+                id='name'
+                name='name'
+                value={name}
+                placeholder='Enter name here'
+                onChange={handleChange}
+                className='mt-1 p-2 border rounded-md w-full'
+              />
+            </div>
 
-export default Form
+            <div>
+              <label htmlFor='amount' className='block text-sm font-medium text-gray-700'>
+                Amount
+              </label>
+              <input
+                id='amount'
+                name='amount'
+                type='number'
+                value={amount}
+                placeholder='Enter amount here'
+                onChange={handleChange}
+                className='mt-1 p-2 border rounded-md w-full'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='date' className='block text-sm font-medium text-gray-700'>
+                Date
+              </label>
+              <input
+                id='date'
+                name='date'
+                type='text'
+                value={date}
+                placeholder='dd/mm/yyyy'
+                onChange={handleChange}
+                className='mt-1 p-2 border rounded-md w-full'
+              />
+            </div>
+            <div className='flex gap-2 justify-center'>  
+              <button
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                onClick={handleAdd}
+              >
+                Add
+              </button>
+              <button
+                className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+}
