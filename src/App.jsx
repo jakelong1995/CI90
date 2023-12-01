0e71605a49ea58f53b70fbd9e09a569f9d15d4f6
jakelong1995 <hoanglong2579@gmail.com>
2023-12-01 09:50:59 +0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Appcontainer from './BigAssignment/Appcontainer'
+import Slots from './Slots'
 
 const App = () => {
   return (
-    <Appcontainer/>
+      // <Appcontainer/>
+      <Slots val1='7️⃣' val2='7️⃣' val3='7️⃣'/>
+    
 
   )
 }
---
+++ src/Slots.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Slots({val1,val2,val3}) {
+  const 
+  return (
+    <div>
+      <h1>{val1} {val2} {val3}</h1>
+      <h2></h2>
+    </div>
+  )
+}
