5df09549bc4f502fc4bf2254d19268f61cb4160f
jakelong1995 <hoanglong2579@gmail.com>
2023-12-04 22:10:07 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ const App = () => {
   return (
     // <Appcontainer/>
     // <Slots val1='7️⃣' val2='7️⃣' val3='7️⃣'/>
-    // <ShoppingListApp/>
-    <div>hello</div>
+    <ShoppingListApp/>
   )
 }
 
--- src/ShoppingList/ShoppingListApp.jsx
+++ src/ShoppingList/ShoppingListApp.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ShoppingList from './ShoppingList'
 
+const data = [
+  {id: 1, name: 'eggs', quantity: 12, completed: false},
+  {id: 2, name: 'beef', quantity: 3, completed: true},
+  {id: 3, name: 'chicken', quantity: 2, completed: true},
+  {id: 4, name: 'veggie', quantity: 1, completed: false}
+]
 
 const ShoppingListApp = () => {
-  const data = [
-    {id: 1, name: 'eggs', quantity: 12, completed: false},
-    {id: 2, name: 'beef', quantity: 3, completed: true},
-    {id: 3, name: 'chicken', quantity: 2, completed: true},
-    {id: 4, name: 'veggie', quantity: 1, completed: false}
-  ]
-  
   return (
     <ShoppingList items={data}/>
   )
