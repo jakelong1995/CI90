5e7ff5d8bc8b94153407f50f2e1883b66af059dd
jakelong1995 <hoanglong2579@gmail.com>
2023-12-20 16:52:46 +0700

    1

--- src/TaskManagement.jsx
+++ src/TaskManagement.jsx
@@ -5,17 +5,17 @@ import {collection, query, orderBy, onSnapshot} from "firebase/firestore"
 
 const TaskManagement = () => {
 
-    useEffect(() => {
-        const taskColRef = query(collection(db, 'task-management'), orderBy('title', 'desc'))
-        onSnapshot(taskColRef, (snapshot) => {
-          console.log(snapshot.docss);
-          setTasks(snapshot.docs.map(doc => ({
-            id: doc.id,
-            data: doc.data()
-          })))
-        })
-      },[])
-
+  useEffect(() => {
+    const taskColRef = query(collection(db, 'task-management'), orderBy('title', 'desc'))
+    onSnapshot(taskColRef, (snapshot) => {
+      console.log(snapshot.docs[0].data());
+      setTasks(snapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+  },[])
+  
   return (
     <div>TaskManagement</div>
   )
