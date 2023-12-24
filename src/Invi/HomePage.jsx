924769b8549a674a00ebe67371de0927c1b3e76b
jakelong1995 <hoanglong2579@gmail.com>
2023-12-24 20:35:16 +0700

    24/12/23

--- src/Invi/HomePage.jsx
+++ src/Invi/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import phoneLightVideo from '/Users/longtmh/Documents/GitHub/CI90/src/Invi/assets/phone-light.mp4';
 import logo from '/Users/longtmh/Documents/GitHub/CI90/src/Invi/assets/InviSm.svg';
-import { Button } from 'antd';
+import logopng from '/Users/longtmh/Documents/GitHub/CI90/src/Invi/assets/Invi.png';
 
 const homepage = () => {
   return (
@@ -11,17 +11,17 @@ const homepage = () => {
             {/* navbar */}
             <div className="w-full justify-between items-center inline-flex">
                 <img src={logo} alt="logo" className='w-10'/>
-                <Button className=''>Sign In</Button>
+                <button className='px-4 py-2 bg-black bg-opacity-5 hover:bg-gray-500 hover:text-white hover: border-none rounded-full inline-flex text-gray-500 text-sm font-semibold'>Sign In</button>
             </div>  
 
             {/* body */}
             <div className='flex flex-col md:flex-row gap-16 justify-between w-full'>
 
                 <div className='flex flex-col max-w-sm gap-8 items-center text-center md:items-start md:text-left mx-auto'>
-                    <img src={logo} alt="logo" className='w-16'/>
+                    <img src={logopng} alt="logo" className='w-16'/>
                     <h1 className='text-gray-800 font-semibold text-4xl md:text-5xl'>Delightful events <span className='text-rose-400'>start here.</span> </h1>
                     <p className='text-gray-600'>Set up an event page, invite friends and sell tickets. Host a memorable event today.</p>
-                    <button type='primary' className='w-fit'>Create Your First Event</button>
+                    <button className='px-6 py-2 bg-gray-800 rounded-lg text-white text-lg font-medium hover:bg-gray-600 border-none'>Create Your First Event</button>
                 </div>
                 
                 <video autoPlay muted className='w-full md:max-w-lg md:mx-auto'>
@@ -30,11 +30,12 @@ const homepage = () => {
         </video>
             </div>
 
-            <div>
-                
+            <div className="w-full border-t border-gray-800 border-opacity-10 gap-4 inline-flex">
+    
             </div>
         </div>
     </div>
+
   )
 }
 
